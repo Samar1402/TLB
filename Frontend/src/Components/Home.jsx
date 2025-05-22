33caca32a2cf54ace77bb613ca276f58f74d7a2d
Samarjeet <samarjeetpandey1402@gmail.com>
2025-05-22 23:24:54 +0530

    add header and footer page

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             We Are Ready To <span className="text-yellow-200">Deliver</span>{" "}
             Your F☺️☺️d 👉🏻
           </h1>
-          <button className="border-black bg-teal-700 text-white px-6 py-2 rounded-2xl w-36 h-14 justify-center font-semibold ">
+          <button className="border-black bg-teal-700 text-white px-6 py-2 rounded-2xl w-36 h-14 justify-center font-semibold">
             ORDER NOW
           </button>
         </div>
